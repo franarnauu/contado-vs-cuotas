449f45b61707d6b557c587b3b132835b621c3563
Francisco Arnau <franciscoarnaucamporro39@gmail.com>
2023-10-17 12:44:57 -0300

    Modifique el codigo para incluir lo que se pide en la segunda pre entrega

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
+let precioEfectivo, precioCuotas, inflacionMensual, totalCuotas;
+let costoEfectivo, costoCuotas, cuotasPagadas;
+
 function calcularConveniencia() {
-    const precioEfectivo = parseFloat(prompt("Ingrese el precio total en efectivo:"));
-    const precioCuotas = parseFloat(prompt("Ingrese el precio total en cuotas:"));
-    const inflacionMensual = parseFloat(prompt("Ingrese la tasa de inflacion mensual estimada (%):")) / 100;
-    const totalCuotas = parseInt(prompt("Ingrese la cantidad total de cuotas:"));
+    precioEfectivo = parseFloat(prompt("Ingrese el precio total en efectivo:"));
+    precioCuotas = parseFloat(prompt("Ingrese el precio total en cuotas:"));
+    inflacionMensual = parseFloat(prompt("Ingrese la tasa de inflación mensual estimada (%):")) / 100;
+    totalCuotas = parseInt(prompt("Ingrese la cantidad total de cuotas:"));
 
     if (isNaN(precioEfectivo) || isNaN(precioCuotas) || isNaN(inflacionMensual) || isNaN(totalCuotas) || totalCuotas < 1) {
-        alert("Ingrese valores validos para los precios, la inflacion, y la cantidad de cuotas.");
+        alert("Ingrese valores válidos para los precios, la inflación, y la cantidad de cuotas.");
         return;
     }
 
-    let costoEfectivo = precioEfectivo;
-    let costoCuotas = 0;
-    let cuotasPagadas = [];
+    costoEfectivo = precioEfectivo;
+    costoCuotas = 0;
+    cuotasPagadas = [];
 
     for (let i = 1; i <= totalCuotas; i++) {
         const cuotaActual = precioCuotas / totalCuotas;
@@ -20,13 +23,26 @@ function calcularConveniencia() {
         cuotasPagadas.push(cuotaActualAjustada);
     }
 
-    const resultado = `Costo en Efectivo: $${costoEfectivo.toFixed(2)}\nCosto en Cuotas (actualizado por inflacion): $${costoCuotas.toFixed(2)}\nMonto de cada cuota: $${(precioCuotas / totalCuotas).toFixed(2)}\nInflacion Mensual Estimada: ${inflacionMensual * 100}%`;
+    mostrarResultado();
+}
+
+function mostrarResultado() {
+    let resultado = "Resultado:\n\n";
+    resultado += "Costo en Efectivo: $" + costoEfectivo.toFixed(2) + "\n";
+    resultado += "Costo en Cuotas (actualizado por inflación): $" + costoCuotas.toFixed(2) + "\n";
+    resultado += "Monto de cada cuota: $" + (precioCuotas / totalCuotas).toFixed(2) + "\n";
+    resultado += "Inflación Mensual Estimada: " + (inflacionMensual * 100) + "%\n\n";
 
     if (costoEfectivo < costoCuotas) {
-        alert(resultado + "\nConviene pagar en efectivo.");
+        resultado += "Conviene pagar en efectivo.";
     } else {
-        alert(resultado + "\nConviene pagar en cuotas.\n\nDetalles de cada cuota:\n" + cuotasPagadas.map((cuota, index) => `Cuota ${index + 1}: $${cuota.toFixed(2)}`).join("\n"));
+        resultado += "Conviene pagar en cuotas.\n\nDetalles de cada cuota:\n";
+        for (let i = 0; i < cuotasPagadas.length; i++) {
+            resultado += "Cuota " + (i + 1) + ": $" + cuotasPagadas[i].toFixed(2) + "\n";
+        }
     }
+
+    alert(resultado);
 }
 
 calcularConveniencia();
