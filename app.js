1cdac4893b27e985b1905fa5a173011a3cde0cfa
Francisco Arnau <franciscoarnaucamporro39@gmail.com>
2023-10-23 21:02:50 -0300

    Agregue el array para que todo sea mas ordenado y utilice un for each para aplicar lo aprendido

--- app.js
+++ app.js
@@ -1,26 +1,33 @@
-let precioEfectivo, precioCuotas, inflacionMensual, totalCuotas;
-let costoEfectivo, costoCuotas, cuotasPagadas;
+const datos = {
+    precioEfectivo: 0,
+    precioCuotas: 0,
+    inflacionMensual: 0,
+    totalCuotas: 0,
+    costoEfectivo: 0,
+    costoCuotas: 0,
+    cuotasPagadas: [],
+};
 
 function calcularConveniencia() {
-    precioEfectivo = parseFloat(prompt("Ingrese el precio total en efectivo:"));
-    precioCuotas = parseFloat(prompt("Ingrese el precio total en cuotas:"));
-    inflacionMensual = parseFloat(prompt("Ingrese la tasa de inflación mensual estimada (%):")) / 100;
-    totalCuotas = parseInt(prompt("Ingrese la cantidad total de cuotas:"));
+    datos.precioEfectivo = parseFloat(prompt("Ingrese el precio total en efectivo:"));
+    datos.precioCuotas = parseFloat(prompt("Ingrese el precio total en cuotas:"));
+    datos.inflacionMensual = parseFloat(prompt("Ingrese la tasa de inflación mensual estimada (%):")) / 100;
+    datos.totalCuotas = parseInt(prompt("Ingrese la cantidad total de cuotas:"));
 
-    if (isNaN(precioEfectivo) || isNaN(precioCuotas) || isNaN(inflacionMensual) || isNaN(totalCuotas) || totalCuotas < 1) {
-        alert("Ingrese valores válidos para los precios, la inflación, y la cantidad de cuotas.");
+    if (isNaN(datos.precioEfectivo) || isNaN(datos.precioCuotas) || isNaN(datos.inflacionMensual) || isNaN(datos.totalCuotas) || datos.totalCuotas < 1) {
+        alert("Ingrese valores válidos para los precios, la inflación y la cantidad de cuotas.");
         return;
     }
 
-    costoEfectivo = precioEfectivo;
-    costoCuotas = 0;
-    cuotasPagadas = [];
+    datos.costoEfectivo = datos.precioEfectivo;
+    datos.costoCuotas = 0;
+    datos.cuotasPagadas = [];
 
-    for (let i = 1; i <= totalCuotas; i++) {
-        const cuotaActual = precioCuotas / totalCuotas;
-        const cuotaActualAjustada = cuotaActual / Math.pow(1 + inflacionMensual, i);
-        costoCuotas += cuotaActualAjustada;
-        cuotasPagadas.push(cuotaActualAjustada);
+    for (let i = 1; i <= datos.totalCuotas; i++) {
+        const cuotaActual = datos.precioCuotas / datos.totalCuotas;
+        const cuotaActualAjustada = cuotaActual / Math.pow(1 + datos.inflacionMensual, i);
+        datos.costoCuotas += cuotaActualAjustada;
+        datos.cuotasPagadas.push(cuotaActualAjustada);
     }
 
     mostrarResultado();
@@ -28,18 +35,18 @@ function calcularConveniencia() {
 
 function mostrarResultado() {
     let resultado = "Resultado:\n\n";
-    resultado += "Costo en Efectivo: $" + costoEfectivo.toFixed(2) + "\n";
-    resultado += "Costo en Cuotas (actualizado por inflación): $" + costoCuotas.toFixed(2) + "\n";
-    resultado += "Monto de cada cuota: $" + (precioCuotas / totalCuotas).toFixed(2) + "\n";
-    resultado += "Inflación Mensual Estimada: " + (inflacionMensual * 100) + "%\n\n";
+    resultado += "Costo en Efectivo: $" + datos.costoEfectivo.toFixed(2) + "\n";
+    resultado += "Costo en Cuotas (actualizado por inflación): $" + datos.costoCuotas.toFixed(2) + "\n";
+    resultado += "Monto de cada cuota: $" + (datos.precioCuotas / datos.totalCuotas).toFixed(2) + "\n";
+    resultado += "Inflación Mensual Estimada: " + (datos.inflacionMensual * 100) + "%\n\n";
 
-    if (costoEfectivo < costoCuotas) {
+    if (datos.costoEfectivo < datos.costoCuotas) {
         resultado += "Conviene pagar en efectivo.";
     } else {
         resultado += "Conviene pagar en cuotas.\n\nDetalles de cada cuota:\n";
-        for (let i = 0; i < cuotasPagadas.length; i++) {
-            resultado += "Cuota " + (i + 1) + ": $" + cuotasPagadas[i].toFixed(2) + "\n";
-        }
+        datos.cuotasPagadas.forEach((cuota, index) => {
+            resultado += "Cuota " + (index + 1) + ": $" + cuota.toFixed(2) + "\n";
+        });
     }
 
     alert(resultado);
